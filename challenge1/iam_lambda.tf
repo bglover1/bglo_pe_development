10a4c6b4f2c5c64fb88af9e1ce45053ea35f82ec
bglover1 <bglover@redventures.com>
2022-03-07 10:58:43 -0500

    Update iam_lambda.tf

--- challenge1/iam_lambda.tf
+++ challenge1/iam_lambda.tf
@@ -13,7 +13,7 @@ resource "aws_iam_role" "challenge1_lambda" {
         Effect = "Allow"
         Sid    = ""
         Principal = {
-          Service = "ec2.amazonaws.com"
+          Service = // change service "ec2.amazonaws.com"
         }
       },
     ]
