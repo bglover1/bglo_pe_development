24c91f05e4d482602f8fe464727b279d22fccff1
bglover1 <bglover@redventures.com>
2022-03-07 10:34:34 -0500

    Update iam_lambda.tf

--- challenge1/iam_lambda.tf
+++ challenge1/iam_lambda.tf
@@ -1,2 +1,20 @@
-//for iam to run lambda
+resource "aws_iam_role" "challenge1_lambda" {
+  name = "challenge1_lambda"
   
+  # I got the stuff below from the website and im not sure what it means. 
+  
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
