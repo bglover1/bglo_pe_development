3c246e0873e163810802a559a8243eb5d743304a
bglover1 <bglover@redventures.com>
2022-03-07 11:18:31 -0500

    Update challenge1_lambda.tf

--- challenge1/challenge1_lambda.tf
+++ challenge1/challenge1_lambda.tf
@@ -1,1 +1,9 @@
-//run challenge1.zip lambda
+resource "aws_lambda_function" "challenge1" {
+// ^^ That's the only part I'm confident about ^^
+  s3_bucket = // output bucket "my_bucket"
+  s3_key    = // should this be a variable? "challenge1.zip"
+  function_name    = "challenge1"
+  handler          = // i dont really understand what a handler is "module.handler"
+  runtime          = "python3.6"
+  timeout          = 180
+}
