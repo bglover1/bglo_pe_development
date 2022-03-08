efc1d430021f8da4dae4aacba0353427e88a1ade
bglover1 <bglover@redventures.com>
2022-03-08 08:54:34 -0500

    Update challenge1_lambda.tf

--- challenge1/challenge1_lambda.tf
+++ challenge1/challenge1_lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "challenge1" {
 // ^^ That's the only part I'm confident about ^^
-  s3_bucket = // output bucket "my_bucket"
-  s3_key    = // should this be a variable? "challenge1.zip"
+//  s3_bucket = // output bucket "my_bucket" // This is causing an error in terraform init
+//  s3_key    = // should this be a variable? "challenge1.zip"
   function_name    = "challenge1"
   handler          = // i dont really understand what a handler is "module.handler"
   runtime          = "python3.6"
