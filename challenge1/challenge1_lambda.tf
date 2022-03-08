a6575a23df62c8026013adfea017aeac517a7d97
bglover1 <bglover@redventures.com>
2022-03-08 08:56:50 -0500

    Update challenge1_lambda.tf

--- challenge1/challenge1_lambda.tf
+++ challenge1/challenge1_lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "challenge1" {
 //  s3_bucket = // output bucket "my_bucket" // This is causing an error in terraform init
 //  s3_key    = // should this be a variable? "challenge1.zip"
   function_name    = "challenge1"
-  handler          = // i dont really understand what a handler is "module.handler"
+//  handler          = // i dont really understand what a handler is "module.handler" // this is causing an error in terraform init
   runtime          = "python3.6"
   timeout          = 180
 }
