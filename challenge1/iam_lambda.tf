6cfd0db7be2342d46c3af425522bcbe2445dac4f
bglover1 <bglover@redventures.com>
2022-03-08 08:21:00 -0500

    Update iam_lambda.tf

--- challenge1/iam_lambda.tf
+++ challenge1/iam_lambda.tf
@@ -1,20 +1,40 @@
-resource "aws_iam_role" "challenge1_lambda" {
-  name = "challenge1_lambda"
-  
-  # I got the stuff below from the website and im not sure what it means. 
-  
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = // change service "ec2.amazonaws.com"
-        }
+// from https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
+resource "aws_iam_role" "iam_challenge1_lambda" {
+  name = "iam_challenge1_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
       },
-    ]
-  })
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "challenge1_lambda" {
+  filename      = "challenge1.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_challenge1_lambda.arn
+  handler       = "index.test"
+
+//  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+//  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+//  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+
+  runtime = "nodejs12.x"
+
+//  environment {
+//    variables = {
+//      foo = "bar"
+    }
+  }
+}
