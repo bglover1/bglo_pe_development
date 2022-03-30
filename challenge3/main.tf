c341b9799186037bc56b32c2d3b16920ddca96ef
bglover1 <bglover@redventures.com>
2022-03-30 10:44:58 -0400

    Update main.tf

--- challenge3/main.tf
+++ challenge3/main.tf
@@ -9,8 +9,32 @@ resource "aws_ssm_parameter" "secret" {
   description = "The parameter description"
   type        = "SecureString"
   value       = var.database_master_password
+}
 
-  tags = {
-    environment = "production"
-  }
+resource "aws_iam_role" "lambda_exec" {
+  name = "challenge1_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "challenge3" {
+  filename      = "challenge-03.zip"
+  function_name = "Bens-Function"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "challenge-03"
+
+  source_code_hash = filebase64sha256("challenge-03.zip")
+
+  runtime = "go1.x"
 }
