00ddaea44323f22a9d600eeef50bfbf75dd176a7
bglover1 <bglover@redventures.com>
2022-04-30 09:03:48 -0400

    Update main.tf

--- challenge5/main.tf
+++ challenge5/main.tf
@@ -147,29 +147,3 @@ resource "aws_security_group" "allow_egress_all" {
   }
 }
 
-resource "aws_subnet" "bensrds" {
-  vpc_id     = "vpc-06d3f8ac2206caa8b"
-  cidr_block = "100.67.16.0/22"
-
-  tags = {
-    Name = "rv-sandbox"
-  }
-}
-
-resource "aws_db_subnet_group" "bensrds_sg" {
-  name       = "bensrds"
-  subnet_ids = data.aws_subnet_ids.bensrds.ids
-
-  tags = {
-    Name = "Challenge5 RDS"
-  }
-}
-
-resource "aws_db_security_group" "rds" {
-  name = "rds_sg"
-
-  ingress {
-    cidr = "100.67.16.0/22"
-  }
-}
-
