3c87fa235fa058adffed0ba7e13842b5db508815
bglover1 <bglover@redventures.com>
2022-04-30 09:28:31 -0400

    Update main.tf

--- challenge5/main.tf
+++ challenge5/main.tf
@@ -114,7 +114,10 @@ resource "aws_ecs_task_definition" "demo-ecs-task-definition" {
                "awslogs-region":"us-east-1",
                "awslogs-stream-prefix":"ecs"
             }
-      }
+      },
+      "environment": [
+       {"name": "DB_HOST", "value": aws_db_instance.bensrds.address,  }
+      ],
   }
 ]
 EOF
