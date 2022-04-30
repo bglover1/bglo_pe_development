cece6e9290bc262ebc992c22deac8681f2784a12
bglover1 <bglover@redventures.com>
2022-04-30 09:36:33 -0400

    Update variables.tf

--- challenge5/variables.tf
+++ challenge5/variables.tf
@@ -2,10 +2,12 @@ variable "db_password" {
   description = "RDS root user password"
   type        = string
   sensitive   = true
+  default     = "changeme"
 }
 
 variable "user_name" {
   description = "RDS username"
   type        = string
   sensitive   = true
+  default     = "root"
 }
