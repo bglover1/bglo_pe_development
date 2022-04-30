a02eeb11a27b220b71ef80b9e886c786f575c3e8
bglover1 <bglover@redventures.com>
2022-04-30 07:48:24 -0400

    Update rds.tf

--- challenge5/rds.tf
+++ challenge5/rds.tf
@@ -12,3 +12,20 @@ resource "aws_db_instance" "bensrds" {
   publicly_accessible    = true
   skip_final_snapshot    = true
 }
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+resource "aws_db_security_group" "default" {
+  name = "rds_sg"
+
+  ingress {
+    cidr = "10.0.0.0/24"
+  }
+}
